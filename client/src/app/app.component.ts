f9915b0f4dfa520e690abee8f87fb43c67966650
José Batista <jose.batista@dfo.global>
2021-04-12 20:52:30 -0300

    Partial

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { IProduct } from './modules/product';
-import { IPagination } from './modules/pagination';
+import { Component } from '@angular/core';
+
+//declare var LeaderLine: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Skinet';
-  products: IProduct[];
+  
+  constructor() {}
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    })
-  }
+  //tipos: number[] = [1,2,3];
+  //@ViewChild('startingElement', { read: ElementRef }) startingElement: ElementRef;
+  //@ViewChild('endingElement', { read: ElementRef }) endingElement: ElementRef;
+  //@ViewChild('endingElement1', { read: ElementRef }) endingElement1: ElementRef;
+  // ngAfterViewInit(): void {
+  //   // const line = new LeaderLine(this.startingElement.nativeElement, this.endingElement.nativeElement);
+  //   // const line1 = new LeaderLine(this.endingElement.nativeElement, this.endingElement1.nativeElement);
+  //   // new LeaderLine(
+  //   //   document.getElementById('start'),
+  //   //   document.getElementById('end')
+  //   // );
+  // }
 }
