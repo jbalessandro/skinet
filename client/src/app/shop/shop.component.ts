1a347585ceb4766ac04b2f5f3b6b36a81e259d2d
José Batista <jose.batista@dfo.global>
2021-04-27 20:40:45 -0300

    end of section 9

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IBrand } from '../shared/modules/brands';
 import { IProduct } from '../shared/modules/product';
 import { IType } from '../shared/modules/productType';
@@ -11,6 +11,7 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
+  @ViewChild('search', { static: true }) searchTerm: ElementRef;
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
@@ -59,21 +60,37 @@ export class ShopComponent implements OnInit {
 
     onBrandSelected(brandId: number) {
       this.shopParams.brandId = brandId;
+      this.shopParams.pageNumber = 1;
       this.getProducts();
     }
 
     onTypeSelected(typeId: number) {
       this.shopParams.typeId = typeId;
+      this.shopParams.pageNumber = 1;
       this.getProducts();
     }
 
     onSortSelected(sort: string) {
       this.shopParams.sort = sort;
+      this.shopParams.pageNumber = 1;
       this.getProducts();
     }
 
     onPageChanged(event: any) {
-      this.shopParams.pageNumber = event.page;
+      if (this.shopParams.pageNumber !== event){
+        this.shopParams.pageNumber = event;
+        this.getProducts();  
+      }
+    }
+
+    onSearch() {
+      this.shopParams.search = this.searchTerm.nativeElement.value;
+      this.getProducts();
+    }
+
+    onReset() {
+      this.searchTerm.nativeElement.value = '';
+      this.shopParams = new ShopParams();
       this.getProducts();
     }
 }
